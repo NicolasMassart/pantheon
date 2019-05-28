73ed1d56bf5d38e3cb5ea21b8af87855f5fefab6
Nicolas MASSART <nicolas.massart@consensys.net>
2019-05-28 13:28:29 +0200

    fix test run command

--- kubernetes/test.sh
+++ kubernetes/test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/bash
 
 export GOSS_PATH=tests/goss-linux-amd64
 export GOSS_OPTS="$GOSS_OPTS --format junit"
@@ -9,7 +9,7 @@ i=0
 
 # Test for normal unconfigured startup
 GOSS_FILES_PATH=tests/01 \
-sh tests/dgoss \
+bash tests/dgoss \
 run $DOCKER_IMAGE \
 --network=dev \
 --rpc-http-enabled \
